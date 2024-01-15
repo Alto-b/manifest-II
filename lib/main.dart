c2d24e6bde3391d8e871d354252e56b14d1f2b61
Alto b <altob282@gmail.com>
2024-01-15 17:37:39 +0530

    v.2

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:manifest2/helper/student_helper.dart';
+import 'package:manifest2/model/student_model.dart';
+import 'package:manifest2/screens/homepage.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async{
+
+  
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+   Hive.openBox<StudentModel>('student_db');
+
+  if(!Hive.isAdapterRegistered(StudentModelAdapter().typeId)){
+    Hive.registerAdapter(StudentModelAdapter());
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          // centerTitle: true,
+          iconTheme: IconThemeData(color: Colors.black54),
+          titleTextStyle: GoogleFonts.roboto(color: Colors.black54),
+          elevation: 0
+        ), colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(background: Colors.white)
+      ),
+      home: const HomePage(),
+    );
+  }
+}
